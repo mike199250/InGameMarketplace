74beb8de98ba0a22b25cd6ed6eb4c1b3dfee6fd1
mike199250 <mike199250@gmail.com>
2026-07-09 16:34:56 +0800

    feat: add secrets/secrets.json to Configuration

--- server/src/Marketplace.Shared/Configuration/ConfigurationConfigurator.cs
+++ server/src/Marketplace.Shared/Configuration/ConfigurationConfigurator.cs
@@ -11,6 +11,7 @@ internal class ConfigurationConfigurator(ServerPaths serverPaths) : IHostApplica
 		LoadEnvLocalInDevelopment(builder.Environment);
 
 		builder.Configuration
+			.AddJsonFile(Path.Combine(serverPaths.ServerRoot, "secrets/secrets.json"), optional: true)
 			.AddEnvironmentVariables()
 			;
 	}

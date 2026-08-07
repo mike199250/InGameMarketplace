5698dd9e930164b8f4e21bc30c0e52d36c2bde87
mike199250 <mike199250@gmail.com>
2026-08-07 19:08:08 +0800

    feat(item): add UserItem table

---
+++ server/src/Marketplace.Migrations/Migrations/20260807092316_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marketplace.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ItemId = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserItem", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserItem_OwnerId_ItemId",
+                table: "UserItem",
+                columns: new[] { "OwnerId", "ItemId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserItem");
+        }
+    }
+}
